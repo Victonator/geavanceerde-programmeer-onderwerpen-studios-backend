27915ae5f8e463b42957aedf1e794ebe7cb24272
Victonator <victorwelters@gmail.com>
2021-12-26 17:25:57 +0100

    initial commit

---
+++ src/test/java/fact/it/studioservice/StudioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fact.it.studioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
